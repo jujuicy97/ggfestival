2b70ecc75d6320e1c12846e7566f0b73f707e2a7
DODAM <64620695+kimhyunseo@users.noreply.github.com>
2025-09-14 15:49:02 +0900

    0914 hs

---
+++ src/components/Popup.js
@@ -0,0 +1,32 @@
+// 아래처럼 사용해주시면 됩니다!!
+
+// const [showPopup, setShowPopup] = useState(false);
+
+//   {showPopup && (
+  //     <Popup 
+  //       popup
+  //       mainText="메인 텍스트" 
+  //       subText="서브 텍스트" 
+  //       btnText="버튼 텍스트" // 기본값: "확인" (btnText 자체를 안 적으면 확인으로 나옵니다! "확인"이 아닐 경우에만 적어주시면 돼요)
+  //       onClose={() => setShowPopup(false)}
+  //     />
+  //   )}
+
+import { IoAlertCircle } from "react-icons/io5";
+
+const Popup = ( {mainText, subText, btnText = "확인", onClose}) => {
+  return (
+    <div className="popup">
+      <div className="popup-box">
+        <div className="popup-text">
+          <IoAlertCircle />
+          <h4>{mainText}</h4>
+          <p>{subText}</p>
+        </div>
+        <button onClick={onClose}>{btnText}</button>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
